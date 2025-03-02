1cf6451b06dcefeb3a9f7f0ff7b680727c0b4496
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2025-03-02 16:31:31 +0200

    add action to run gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff from '../src/gendiff.js';
 const program = new Command();
 
 program
@@ -8,6 +9,8 @@ program
   .version('0.0.1')
   .option('-f --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {});
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
 
 program.parse();
