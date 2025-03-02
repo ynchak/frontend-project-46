f3656eee174a405756dc188f15499d45c9d60fd9
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2025-03-02 16:33:09 +0200

    refactor

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,3 +1,18 @@
-const gendiff = (filepath1, filepath2) => {};
+import buildDiff from './parses/ast.js';
+import parse from './parses/index.js';
+import { getData, getExt } from './utils.js';
+
+const loadfile = (filepath) => {
+  const data = getData(filepath);
+  const ext = getExt(filepath);
+  return parse(data, ext);
+};
+
+const gendiff = (filepath1, filepath2) => {
+  const obj1 = loadfile(filepath1);
+  const obj2 = loadfile(filepath2);
+  const diff = buildDiff(obj1, obj2);
+  return diff;
+};
 
 export default gendiff;
