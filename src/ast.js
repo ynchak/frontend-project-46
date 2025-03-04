1850861b574d055ec94ef3ebf2133712185bc09e
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2025-03-04 15:24:08 +0200

    add prop key

--- src/ast.js
+++ src/ast.js
@@ -7,14 +7,14 @@ const buildDiff = (obj1, obj2) => {
     const hasKey1 = Object.hasOwn(obj1, key);
     const hasKey2 = Object.hasOwn(obj2, key);
 
-    if (!hasKey1) return { status: 'added', [key]: obj2[key] };
-    if (!hasKey2) return { status: 'removed', [key]: obj1[key] };
+    if (!hasKey1) return { status: 'added', key, value: obj2[key] };
+    if (!hasKey2) return { status: 'deleted', key, value: obj1[key] };
     if (obj1[key] === obj2[key])
-      return { status: 'unchanged', [key]: obj1[key] };
+      return { status: 'unchanged', key, value: obj1[key] };
 
     return [
-      { status: 'removed', [key]: obj1[key] },
-      { status: 'added', [key]: obj2[key] },
+      { status: 'deleted', key, value: obj1[key] },
+      { status: 'added', key, value: obj2[key] },
     ];
   });
 };
