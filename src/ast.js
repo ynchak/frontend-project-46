310964c09f727d78ee41f7447f6097e6caf83127
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2025-03-18 20:58:02 +0200

    update

--- src/ast.js
+++ src/ast.js
@@ -1,3 +1,5 @@
+import { isObject } from './utils.js';
+
 const buildDiff = (obj1, obj2) => {
   const uniqKeys = [
     ...new Set([...Object.keys(obj1), ...Object.keys(obj2)]),
@@ -6,16 +8,21 @@ const buildDiff = (obj1, obj2) => {
   return uniqKeys.flatMap((key) => {
     const hasKey1 = Object.hasOwn(obj1, key);
     const hasKey2 = Object.hasOwn(obj2, key);
+    const value1 = obj1[key];
+    const value2 = obj2[key];
 
-    if (!hasKey1) return { status: 'added', key, value: obj2[key] };
-    if (!hasKey2) return { status: 'deleted', key, value: obj1[key] };
-    if (obj1[key] === obj2[key])
-      return { status: 'unchanged', key, value: obj1[key] };
+    if (!hasKey1) return [{ status: 'added', key, value: value2 }];
+    if (!hasKey2) return [{ status: 'deleted', key, value: value1 }];
 
-    return [
-      { status: 'deleted', key, value: obj1[key] },
-      { status: 'added', key, value: obj2[key] },
-    ];
+    if (isObject(value1) && isObject(value2)) {
+      return [{ status: 'nested', key, children: buildDiff(value1, value2) }];
+    }
+
+    if (value1 !== value2) {
+      return [{ status: 'updated', key, from: value1, to: value2 }];
+    }
+
+    return [{ status: 'unchanged', key, value: value1 }];
   });
 };
 export default buildDiff;
